e96e83d582f2faceb0173dcaa102d94f049a93c2
Mohammed Shahhat <mohammed@mshahhat.com>
2025-08-29 01:09:44 -0400

    First commit

---
+++ controllers/user.js
@@ -0,0 +1,4 @@
+import service from "../services/user.js";
+
+
+export { getUser };
---
+++ utils/error.js
@@ -0,0 +1,22 @@
+class CustomError extends Error {
+  constructor({ statusCode, message }) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
+const handleError = (err, req, res, next) => {
+  let { statusCode, message } = err;
+  console.error(message);
+  if (!statusCode) {
+    statusCode = 500; // Default to 500 if no status code is provided
+  }
+  res.status(statusCode).json({
+    status: "error",
+    statusCode,
+    message,
+  });
+};
+
+export { handleError, CustomError };
